4ba2df208c460e84c4a71e966f2276c1ffece9cc
Chris Arnesen <chris.arnesen@gmail.com>
2016-08-27 15:38:23 -0500

    Add a bunch of features (#1)
    * -a
    
    WIP
    
    * wip
    
    * Is in good shape

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,9 +1,35 @@
+'use strict';
 
 require('chai').should();
 
-const util = require('../index');
+const util = require('../..');
+
+function succeed(arg1, arg2, callback) {
+  callback(null, arg1, arg2);
+}
+
+function fail(arg, callback) {
+  callback(new Error(arg));
+}
 
 describe('tests', function () {
+
+  it('isArray return true if an array is passed', function () {
+    util.isArray([]).should.equal(true);
+  });
+
+  it('isArray return false if a non-array is passed', function () {
+    util.isArray().should.equal(false);
+    util.isArray('foo').should.equal(false);
+    util.isArray({}).should.equal(false);
+    util.isArray(null).should.equal(false);
+  });
+
+  it('firstDefined returns the first argument that is not undefined', function () {
+    (util.firstDefined() === undefined).should.equal(true);
+    util.firstDefined(undefined, 'foo').should.equal('foo');
+    util.firstDefined(undefined, 0, undefined).should.equal(0);
+  });
 
   it('isDefined return false if value is undefined', function () {
     util.isDefined().should.equal(false);
@@ -34,19 +60,11 @@ describe('tests', function () {
 
   it('promisify returns a promise that is fulfilled on success', function () {
 
-    function succeed(arg1, arg2, callback) {
-      callback(null, [arg1, arg2]);
-    }
-
     return util.promisify(succeed)('foo', 'bar')
-      .then(ret => ret.should.eql(['foo', 'bar']));
+      .then(ret => ret.should.eql('foo'));
   });
 
   it('promisify returns a promise that is rejected on error', function () {
-
-    function fail(arg, callback) {
-      callback(new Error(arg));
-    }
 
     return util.promisify(fail)('foo')
       .then(() => {
@@ -56,6 +74,11 @@ describe('tests', function () {
       .catch(err => {
         err.message.should.equal('foo');
       });
+  });
+
+  it('promisify returns an array if returnArray is true', function () {
+    return util.promisify(succeed, { returnArray: true })('foo', 'bar')
+      .then(ret => ret.should.eql(['foo', 'bar']));
   });
 
 });
--- src/index.js
+++ src/index.js
@@ -1,33 +1,160 @@
 'use strict';
 
+function isArray(value) {
+  return Array.isArray(value);
+}
+
+function isBoolean(value) {
+  return typeof value === 'boolean';
+}
+
 function isDefined(value) {
-  return typeof value !== 'undefined';
+  return !isUndefined(value);
+}
+
+function isEmptyObject(value) {
+  return isObject(value) && Object.keys(value).length === 0 && value.constructor === Object;
+}
+
+function isFunction(value) {
+  return typeof value === 'function';
+}
+
+function isObject(value) {
+  return value === Object(value);
+}
+
+function isString(value) {
+  return typeof value === 'string';
+}
+
+function makeExpectedError(name, typeString) {
+  const an = startsWithVowel(typeString) ? 'an' : 'a';
+  const message = `Expected argument "${name}" to be ${an} ${typeString}`;
+  return new Error(message);
+}
+
+function startsWithVowel(str) {
+  return (/^[aeiou]$/i).test(str);
+}
+
+function isUndefined(value) {
+  return typeof value === 'undefined';
 }
 
 module.exports = {
 
-  isDefined,
-
-  isFunction(value) {
-    return typeof value === 'function';
-  },
-
-  throwIfUndefined(value, name = 'value') {
-    if (!isDefined(value)) {
-      throw new Error('Expected ' + name + ' to be defined');
+  firstDefined(...args) {
+    for (let arg of [...args]) {
+      if (isDefined(arg)) {
+        return arg;
+      }
     }
   },
 
-  promisify(func) {
+  isArray,
+
+  isBoolean,
+
+  isDefined,
+
+  isEmptyObject,
+
+  isFunction,
+
+  isString,
+
+  isUndefined,
+
+  promisify(func, options) {
+    options = options || {};
     return (...args) => new Promise((resolve, reject) => {
-      func(...args, (err, ret) => {
+      func(...args, (err, ...rets) => {
         if (err) {
           reject(err);
         } else {
-          resolve(ret);
+          if (options.returnArray) {
+            resolve([...rets]);
+          } else {
+            resolve([...rets][0]);
+          }
         }
       });
     });
+  },
+
+  startsWithVowel,
+
+  throwIf(condition, message) {
+    if (condition) {
+      throw new Error(message);
+    }
+  },
+
+  throwIfEmptyObject(value, name) {
+    if (isEmptyObject(value)) {
+      throw makeExpectedError(name, 'not to be an empty object');
+    }
+  },
+
+  throwIfNot(condition, message) {
+    if (!condition) {
+      throw new Error(message);
+    }
+  },
+
+  throwIfNotArray(value, name = 'value') {
+    if (!isArray(value)) {
+      throw makeExpectedError(name, 'array');
+    }
+  },
+
+  throwIfNotBoolean(value, name = 'value') {
+    if (!isBoolean(value)) {
+      throw makeExpectedError(name, 'boolean');
+    }
+  },
+
+  throwIfNotFunction(value, name = 'value') {
+    if (!isFunction(value)) {
+      throw makeExpectedError(name, 'function');
+    }
+  },
+
+  throwIfNotObject(value, name = 'value') {
+    if (!isObject(value)) {
+      throw makeExpectedError(name, 'object');
+    }
+  },
+
+  throwIfNotString(value, name = 'value') {
+    if (!isString(value)) {
+      throw makeExpectedError(name, 'string');
+    }
+  },
+
+  throwIfNotNonEmptyObject(value, name = 'value') {
+    if (!(isObject(value) && !isEmptyObject(value))) {
+      throw makeExpectedError(name, 'non-empty object');
+    }
+  },
+
+  throwIfNotPositiveLengthString(value, name = 'value') {
+    if (!(isString(value) && value.length > 0)) {
+      throw makeExpectedError(name, 'string with length > 0');
+    }
+  },
+
+  throwIfDefined(value, name = 'value') {
+    if (isDefined(value)) {
+      throw makeExpectedError(name, 'undefined value');
+    }
+  },
+
+  throwIfUndefined(value, name = 'value') {
+    if (isUndefined(value)) {
+      throw makeExpectedError(name, 'defined value');
+    }
   }
 
 };
