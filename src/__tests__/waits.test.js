05083d1f4bc0b667d9d4ef71f8233fb74adf5219
Chris Arnesen <chris.arnesen@gmail.com>
2016-08-28 17:58:30 -0500

    Split checks, add waits (#2)

---
+++ src/__tests__/waits.test.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const { waitForEvent, waitForNonEvent } = require('../waits');
+
+const interval = 50;
+const data = 'foo';
+
+const emitter = {
+  once(name, callback) {
+    setTimeout(() => callback('foo'), interval);
+  },
+  removeListener() {}
+};
+
+describe('waitForEvent', function (){
+
+  it('returns a promise that resolves later', function () {
+    const initialTime = Date.now();
+    return waitForEvent(emitter, 'does not matter')
+      .then(ret => {
+        const elapsedTime = Date.now() - initialTime;
+        elapsedTime.should.be.above(30);
+        elapsedTime.should.be.below(100);
+        ret.should.eql(data);
+      });
+  });
+
+  it('returns a promise that rejects if interval is set', function () {
+    const initialTime = Date.now();
+    return waitForEvent(emitter, 'does not matter', interval / 2)
+      .then(() => {
+        throw new Error('An error message that does not start with "Timed out"');
+      })
+      .catch(err => {
+        const elapsedTime = Date.now() - initialTime;
+        elapsedTime.should.be.above(interval / 4);
+        elapsedTime.should.be.below(interval);
+        err.message.startsWith('Timed out').should.equal(true);
+      });
+
+  });
+
+});
+
+describe('waitForNonEvent', function (){
+
+  it('returns a promise that rejects if an event arrives', function () {
+    const initialTime = Date.now();
+    return waitForNonEvent(emitter, 'does not matter', interval * 2)
+      .then(() => {
+        throw new Error('An error message that is not the event data');
+      })
+      .catch(ret => {
+        const elapsedTime = Date.now() - initialTime;
+        elapsedTime.should.be.above(30);
+        elapsedTime.should.be.below(100);
+        ret.should.eql(data);
+      });
+  });
+
+  it('returns a promise that resolves if the interval times out', function () {
+    const initialTime = Date.now();
+    return waitForNonEvent(emitter, 'does not matter', interval / 2)
+      .then(() => {
+        const elapsedTime = Date.now() - initialTime;
+        elapsedTime.should.be.above(interval / 4);
+        elapsedTime.should.be.below(interval);
+      });
+
+  });
+
+});
