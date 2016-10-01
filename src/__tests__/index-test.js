fb59e6df367c0818ecaa01818f826fafbfc77d80
Chris Arnesen <chris.arnesen@gmail.com>
2016-10-01 09:42:07 -0500

    Purge stuff that is now in logger and checks

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -1,4 +1,7 @@
-import { delay, waitForEvent, waitForNonEvent } from '../waits'
+import { should } from 'chai'
+import * as util from '../index'
+
+should()
 
 const interval = 50
 const data = 'foo'
@@ -10,10 +13,41 @@ const emitter = {
   removeListener () {}
 }
 
+function succeed (arg1, arg2, callback) {
+  callback(null, arg1, arg2)
+}
+
+function fail (arg, callback) {
+  callback(new Error(arg))
+}
+
+describe('promisify', function () {
+  it('returns a promise that is fulfilled on success', function () {
+    return util.promisify(succeed)('foo', 'bar')
+      .then(ret => ret.should.eql('foo'))
+  })
+
+  it('returns a promise that is rejected on error', function () {
+    return util.promisify(fail)('foo')
+      .then(() => {
+        // this should not be called
+        throw new Error('Test failed')
+      })
+      .catch(err => {
+        err.message.should.equal('foo')
+      })
+  })
+
+  it('returns an array if returnArray is true', function () {
+    return util.promisify(succeed, { returnArray: true })('foo', 'bar')
+      .then(ret => ret.should.eql(['foo', 'bar']))
+  })
+})
+
 describe('waitForEvent', function () {
   it('returns a promise that resolves later', function () {
     const initialTime = Date.now()
-    return waitForEvent(emitter, 'does not matter')
+    return util.waitForEvent(emitter, 'does not matter')
       .then(ret => {
         const elapsedTime = Date.now() - initialTime
         elapsedTime.should.be.above(30)
@@ -24,7 +58,7 @@ describe('waitForEvent', function () {
 
   it('returns a promise that rejects if interval is set', function () {
     const initialTime = Date.now()
-    return waitForEvent(emitter, 'does not matter', interval / 2)
+    return util.waitForEvent(emitter, 'does not matter', interval / 2)
       .then(() => {
         throw new Error('An error message that does not start with "Timed out"')
       })
@@ -40,7 +74,7 @@ describe('waitForEvent', function () {
 describe('waitForNonEvent', function () {
   it('returns a promise that rejects if an event arrives', function () {
     const initialTime = Date.now()
-    return waitForNonEvent(emitter, 'does not matter', interval * 2)
+    return util.waitForNonEvent(emitter, 'does not matter', interval * 2)
       .then(() => {
         throw new Error('An error message that is not the event data')
       })
@@ -54,7 +88,7 @@ describe('waitForNonEvent', function () {
 
   it('returns a promise that resolves if the interval times out', function () {
     const initialTime = Date.now()
-    return waitForNonEvent(emitter, 'does not matter', interval / 2)
+    return util.waitForNonEvent(emitter, 'does not matter', interval / 2)
       .then(() => {
         const elapsedTime = Date.now() - initialTime
         elapsedTime.should.be.above(interval / 4)
@@ -65,7 +99,7 @@ describe('waitForNonEvent', function () {
 
 describe('delay', function () {
   it('resolves the elapsed time after the specified interval', function () {
-    return delay(50).then(elapsedTime => {
+    return util.delay(50).then(elapsedTime => {
       elapsedTime.should.be.above(30)
       elapsedTime.should.be.below(70)
     })
--- src/index.js
+++ src/index.js
@@ -1,4 +1,101 @@
-export * from './checks'
-export * from './waits'
 
-export { default as createLogger } from './createLogger'
+import { throwIfNotFunction, throwIfNotNonEmptyString,
+  isDefined, throwIfNotPositiveNumber } from '@carnesen/checks'
+
+export function promisify (func, options) {
+  throwIfNotFunction(func, 'func')
+  options = options || {}
+  return (...args) => new Promise((resolve, reject) => {
+    func(...args, (err, ...rets) => {
+      if (err) {
+        reject(err)
+      } else {
+        if (options.returnArray) {
+          resolve([...rets])
+        } else {
+          resolve([...rets][0])
+        }
+      }
+    })
+  })
+}
+
+function attachTimedEventCallback ({ event, timeout }) {
+  throwIfNotFunction(event.emitter.once, 'event.emitter.once')
+  throwIfNotFunction(event.emitter.removeListener, 'event.emitter.removeListener')
+  throwIfNotNonEmptyString(event.name, 'event.name')
+  throwIfNotFunction(event.callback, 'event.callback')
+  throwIfNotFunction(timeout.callback, 'timeout.callback')
+
+  if (isDefined(timeout.interval)) {
+    throwIfNotPositiveNumber(timeout.interval, 'timeout.interval')
+  }
+
+  let timeoutId
+
+  function eventHandler (...args) {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+    event.callback(...args)
+  }
+
+  function timeoutHandler () {
+    event.emitter.removeListener(event.name, eventHandler)
+    timeout.callback()
+  }
+
+  if (timeout.interval) {
+    timeoutId = setTimeout(timeoutHandler, timeout.interval)
+  }
+
+  event.emitter.once(event.name, eventHandler)
+}
+
+export function waitForEvent (emitter, name, interval) {
+  return new Promise((resolve, reject) => {
+    attachTimedEventCallback({
+      event: {
+        emitter,
+        name,
+        callback (value) {
+          resolve(value)
+        }
+      },
+      timeout: {
+        interval,
+        callback () {
+          const message = `Timed out after ${interval} milliseconds waiting for event "${name}"`
+          reject(new Error(message))
+        }
+      }
+    })
+  })
+}
+
+export function waitForNonEvent (emitter, name, interval) {
+  return new Promise((resolve, reject) => {
+    attachTimedEventCallback({
+      event: {
+        emitter,
+        name,
+        callback (value) {
+          const err = new Error(`Emitter emitted event "${name}"`)
+          err.value = value
+          reject(err)
+        }
+      },
+      timeout: {
+        interval,
+        callback: resolve
+      }
+    })
+  })
+}
+
+export function delay (interval) {
+  const startTime = Date.now()
+  return new Promise(resolve =>
+    setTimeout(() => resolve(Date.now() - startTime), interval)
+  )
+}
